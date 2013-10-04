aee061e410de487876218021cd9675bdc432b6fb
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2013-10-04 02:15:12 +0000

    Removing debug code, reordering db migrate

--- Makefile
+++ Makefile
@@ -1,19 +1,23 @@
-default: create_image package
+default: clean create_image package
 
 clean:
 	-docker rmi airdrop/testimage
 	-rm web
+	-rm -rf airdrop
 	
 
-create_image: clean
+create_image:
 	go build web.go
 	docker build -t airdrop/testimage .
+	git clone https://github.com/fkautz/airdrop.git
+	cd airdrop && make
 
 package:
 	-rm -rf archive
 	mkdir archive
 	docker save airdrop/testimage > archive/testimage.tar
 	cp run.sh archive/run.sh
+	cp airdrop/airdrop.tar.gz archive/
 	chmod +x archive/run.sh
 	makeself archive archive.bin package ./run.sh
 
--- run.sh
+++ run.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 docker load < testimage.tar
+if [ ! -d "/root/airdrop" ]
+then
+  ls
+  mkdir airdrop
+  tar -xf airdrop.tar.gz -C airdrop
+  cp -a airdrop /root
+fi
 cd /root/airdrop
 # backup fails if these don't exist, need to fix. workaround for now
 mkdir -p /docker/airdrop/testimage/data
 mkdir -p /docker/airdrop/testimage/logs
 
-/root/airdrop/airdrop.rb stop airdrop/testimage
-/root/airdrop/airdrop.rb backup airdrop/testimage
-/root/airdrop/airdrop.rb start airdrop/testimage "-p 80:80"
+bundle install --deployment
+
+### put logic to send poison pill here, application dependent.
+
+# stop will force the image to shut down after 10 seconds
+bundle exec ruby airdrop.rb migrate
+bundle exec ruby airdrop.rb stop airdrop/testimage
+bundle exec ruby airdrop.rb backup airdrop/testimage
+bundle exec ruby airdrop.rb start airdrop/testimage "-p 80:80"
